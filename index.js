1c1e830bcf5d26fe46ae501a1a2d4a31c541fc61
Luceatscode <lucsw92@hotmail.com>
2023-06-17 15:36:41 -0600

    added svg file

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const { validate } = require('@babel/types');
-const inquirer = require('inquirer')
-import { validateHTMLColorHex } from 'validate-color';
-import { validateHTMLColorName } from 'validate-color';
+const inquirer = require('inquirer');
+const fs = require('fs');
+var svg = require('svg-builder')
+    .width(300)
+    .height(200);
 
-function init() { 
+var validateColor = require('validate-color').default;
+import shapes from './lib/shapes.js';
+
+function isValidColor(color) {
+    return validateColor(color);
+}
+
+function init() {
     inquirer.prompt([
         {
             message: 'What is the text for your logo',
@@ -11,7 +20,7 @@ function init() {
             type: 'input',
             validate: function (answer) {
                 if (answer.length > 3) {
-                    return console.log("You must enter up to 3 characters");
+                    return console.log("\nYou must enter up to 3 characters");
                 }
                 else return true;
             }
@@ -21,8 +30,8 @@ function init() {
             name: 'textcolor',
             type: 'input',
             validate: function (answer) {
-                if (validateColor(answer) == false) {
-                    return console.log("You must choose a valid color or hexadecimal value");
+                if (isValidColor(answer) == false) {
+                    return console.log("\nYou must choose a valid color or hexadecimal value");
 
                 } else return true;
             }
@@ -30,20 +39,50 @@ function init() {
         {
             message: 'What shape would you like for your logo',
             name: 'shape',
-            type: 'input',
-            choices: ['square', 'circle', 'triangle']
+            type: 'list',
+            choices: ['square', new inquirer.Separator(), 'circle', new inquirer.Separator(), 'triangle']
         },
         {
             message: 'What color would you like for your shape',
             name: 'shapecolor',
             type: 'input',
             validate: function (answer) {
-                if (validateColor(answer) == false) {
-                    return console.log("You must choose a valid color or hexadecimal value");
+                if (isValidColor(answer) == false) {
+                    return console.log("\nYou must choose a valid color or hexadecimal value");
                 } else return true;
             }
         },
     ]).then(function (data) {
+        var logo = svg;
+
+        if (data.shape == 'square') {
+            logo = new Square(data.text, data.textcolor, data.shapecolor);
+        }
+        else if (data.shape == 'circle') {
+            logo = svg
+            .circle ({
+                fill: data.shapecolor,
+                r: 300,
+                'stroke-width': 1,
+                stroke: data.shapecolor,
+                cx: 42,
+                cy:82,
+            }).text({x: 10, y: 20, stroke: data.textcolor, fill: data.textcolor, 'font-size': 100}, data.text).render();}
+            else if (data.shape == 'triangle') {
+                logo = svg
+                    .polygon({
+                        fill: data.shapecolor,
+                        points: '0,0 100,0 50,100',
+                        'stroke-width': 1,
+                        stroke: data.shapecolor,
+                    }).text({x: 10, y: 20, stroke: data.textcolor, fill: data.textcolor, 'font-size': 100}, data.text).render();
+                }
+
+        // Get the SVG string
+        // const svgContent = svg.toString();
+
+        // Save the SVG content to a file
+        fs.writeFileSync('output.svg', logo);
     })
 }
 // Function call to initialize app
